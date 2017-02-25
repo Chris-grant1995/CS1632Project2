04a3a5519fbca6fcde8e99758646c8f9b524c0e8
Andrew JS Maguire <ajm221@pitt.edu>
2017-02-25 16:33:35 -0500

    Added 1000 ms sleep at the top of the GUIGameLoop
    This should help prevent both players being told the first turn is theirs.

--- src/edu/pitt/battleshipgame/client/Client.java
+++ src/edu/pitt/battleshipgame/client/Client.java
@@ -400,7 +400,7 @@ public class Client extends Application {
                 
                 //GUIGameLoop();
                 while(!gi.isGameOver()){
-
+                    Thread.sleep(1000);
                     updateMessage("Waiting for your turn");
 
                     gi.wait(myPlayerID);
